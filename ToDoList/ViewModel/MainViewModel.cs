d7fa3e53d5fe6e5ad70256ca3792c2db6b3ee927
Andreev <Radricksh@gmail.com>
2023-02-22 21:42:35 +0300

    Add project files.

---
+++ ToDoList/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.ViewModel.Base;
+
+namespace ToDoList.ViewModel
+{
+    internal class MainViewModel : BaseViewModel
+    {
+    }
+}
