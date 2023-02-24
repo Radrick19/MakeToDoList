adc2e7fe191bcb22d3cca77b7070efedd9f1433c
Andreev <Radricksh@gmail.com>
2023-02-24 14:06:52 +0300

    Сделал темплейт для таскайтема

---
+++ ToDoList/Model/ToDoModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ToDoList.Model
+{
+    internal class ToDoModel
+    {
+        public string Task
+        {
+            get { return task; }
+            set { task = value; }
+        }
+
+        public bool IsDone
+        {
+            get { return isDone; }
+            set { isDone = value; }
+        }
+        private string task;
+        private bool isDone;
+
+        public ToDoModel(string task)
+        {
+            Task = task;
+            IsDone = false;
+        }
+    }
+}
--- ToDoList/ViewModel/MainViewModel.cs
+++ ToDoList/ViewModel/MainViewModel.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.Model;
 using ToDoList.ViewModel.Base;
 
 namespace ToDoList.ViewModel
 {
     internal class MainViewModel : BaseViewModel
     {
+        public ObservableCollection<ToDoModel> ToDoCollection { get; set; }
+        private string taskText;
+        public string TaskText
+        {
+            get { return taskText; }
+            set { taskText = value; }
+        }
+        public MainViewModel()
+        {
+            ToDoCollection = new ObservableCollection<ToDoModel>();
+            ToDoCollection.Add(new ToDoModel("Loxasdaskd kasdk;aslkdl; askldk ;askdl ksl;dklask ;kasl; dkasl;k"));
+            ToDoCollection.Add(new ToDoModel("Grox"));
+            ToDoCollection.Add(new ToDoModel("Work"));
+        }
     }
 }
