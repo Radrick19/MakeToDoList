224759f7a2079307d6077acb5bb03343c6df025d
Andreev <Radricksh@gmail.com>
2023-02-24 14:56:57 +0300

    Нужно сделать передачу объекта во вью модель

--- ToDoList/ViewModel/MainViewModel.cs
+++ ToDoList/ViewModel/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using ToDoList.Infrastrucre.Commands.Base;
 using ToDoList.Model;
 using ToDoList.ViewModel.Base;
 
@@ -18,12 +20,34 @@ namespace ToDoList.ViewModel
             get { return taskText; }
             set { taskText = value; }
         }
+        public ICommand AddItem
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+                    if(TaskText != null && TaskText != string.Empty)
+                    {
+                        ToDoCollection.Add(new ToDoModel(TaskText));
+                        Set(ref taskText, string.Empty, nameof(TaskText));
+                    }
+                });
+            }
+        }
+        public ICommand DeleteItem
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+                    ToDoModel model = obj as ToDoModel;
+                    ToDoCollection.Remove(model);
+                });
+            }
+        }
         public MainViewModel()
         {
             ToDoCollection = new ObservableCollection<ToDoModel>();
-            ToDoCollection.Add(new ToDoModel("Loxasdaskd kasdk;aslkdl; askldk ;askdl ksl;dklask ;kasl; dkasl;k"));
-            ToDoCollection.Add(new ToDoModel("Grox"));
-            ToDoCollection.Add(new ToDoModel("Work"));
         }
     }
 }
